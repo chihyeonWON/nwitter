d369ba4b0c3890a73e49990269f7588403d50b97
원치현 <heungeob1003@gmail.com>
2021-08-21 12:39:26 +0900

    데이터상으로 고유id를 줘서 누가 쓴 트윗인지 알 수 있게하는 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,29 @@
 import { dbService } from "fbase";
 import { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
 
     const getNweets = async () => {
         const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document) => 
-            setNweets((prev) => [document.data(), ...prev])
-        );
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id };
+            setNweets((prev) => [nweetObject, ...prev])
+        });
     };
 
     useEffect(() => {
         getNweets();
     }, []);
     
-    console.log(nweets);
+    
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
             text:nweet,
             createAt: Date.now(),
+            creatorId: userObj.uid, 
         });
         setNweet("");
     };
@@ -35,16 +37,25 @@ const Home = () => {
     };
 
     return(
+       <>
         <form onSubmit={onSubmit}>
-            <input
-                value={nweet}
-                onChange={onChange}
-                type="text"
-                placeholder="What's on your mind?"
-                maxLength={120}
-                />
-                <input type="submit" value="Nweet" />
-        </form>
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                    />
+                    <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 
