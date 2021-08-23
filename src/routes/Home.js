8a4ebeaf9684a267c05f2979eb24a121835486dd
원치현 <heungeob1003@gmail.com>
2021-08-23 20:10:13 +0900

    사진 추가 기능 코드

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useEffect, useState } from "react";
 import Nweet from "components/Nweet";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
@@ -20,14 +21,23 @@ const Home = ({ userObj }) => {
     
     const onSubmit = async (event) => {
         event.preventDefault();
+        let attachmentUrl = "";
+        if (attachment !=="") {
+        const attachmentRef = storageService
+                .ref()
+                .child(`${userObj}.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment, "data_url");
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
         await dbService.collection("nweets").add({
-            text:nweet,
-            createAt: Date.now(),
-            creatorId: userObj.uid, 
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+            attachmentUrl,
         });
         setNweet("");
+        setAttachment("");
     };
-
     const onChange = (event) => {
         event.preventDefault();
         const {
