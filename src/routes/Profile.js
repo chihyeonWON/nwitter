959c537c253b8704a3d34585b006ec59f54f42b4
원치현 <heungeob1003@gmail.com>
2021-08-26 19:50:15 +0900

    Profile 컴포넌트에 스타일 적용

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -27,18 +27,24 @@ const Profile = ({ userObj, refreshUser }) => {
     };
 
     return(
-        <>
-            <form onSubmit={onSubmit}>
+        <div className="container">
+            <form onSubmit={onSubmit} className="profileForm">
                 <input
                     onChange={onChange} 
                     type="text" 
                     placeholder="Display name"
                     value={newDisplayName} 
+                    autoFocus
+                    className="formInput"
                 />
-                <input type="submit" value="Update Profile" />
+                <input type="submit" value="Update Profile" className="formBtn" stlye={{
+                    marginTop:10,
+                }} />
             </form>
-            <button onClick={onLogOutClick}>Log Out</button>    
-        </>
+            <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+                Log Out
+            </span>   
+        </div>
     );
 };
 
