86fbb4099daab6553e0ff9222d1fc7250638a1a1
원치현 <heungeob1003@gmail.com>
2021-08-26 19:40:21 +0900

    NweetFactory 컴포넌트에 스타일 적용

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -56,22 +56,45 @@ const NweetFactory = ({ userObj }) => {
     const onClearAttachment = () => setAttachment("");
     
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="factoryForm">
+            <div className="factoryInput__container">
                 <input
+                    className="factory__input" 
                     value={nweet}
                     onChange={onChange}
                     type="text"
                     placeholder="What's on your mind?"
                     maxLength={120}
-                    />
-                    <input type="file" accept="image/*" onChange={onFileChange} />
-                    <input type="submit" value="Nweet" />
-                    {attachment && (
-                        <div>
-                            <img src={attachment} width="50px" height="50px" />
-                            <button onClick={onClearAttachment}>Clear</button>
+                />
+                <input type="submit" value="&rarr;" className="factoryInput__arrow"/>
+            </div>
+            <label htmlFor="attach-file" className="factoryInput__label">
+                <span>Add photos</span>
+                <FontAwesomeIcon icon={faPlus} />
+            </label>
+            <input
+                id="attach-file"
+                type="file"
+                accept="image/*"
+                onChange={onFileChange}
+                style={{
+                    opacity:0,
+                }}
+                />
+                {attachment && (
+                    <div className="factoryForm__attachment">
+                        <img
+                            src={attachment}
+                            style={{
+                                backgroundImage:attachment,
+                            }}
+                        />
+                        <div className="factoryForm__clear" onClick={onClearAttachment}>
+                            <span>Remove</span>
+                            <FontAwesomeIcon icon={faTimes} />
                         </div>
-                    )}     
+                    </div>
+                )}
         </form>
     );
 };
